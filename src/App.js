e57239e37a2fa92c98df1de0fb4fd39f28f6f6a7
i-m-puru <purusaini555@gmail.com>
2022-03-31 01:42:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      {/* Navigation Bar */}
+      <Navbar />
+      <main>
+        {/* TODO: import <Hero/> component here */}
+
+
+        {/* TODO: import <Services/> component here */}
+
+
+        {/* TODO: import <AboutUs/> component here */}
+
+
+        {/* TODO: create a <Gallery/> component in src/components and import it here (OPTIONAL) */}
+
+
+        {/* TODO: import <Testimonial/> component here */}
+
+
+        {/* TODO: import <Footer/> component here */}
+        
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+/* TODO: create hero section here */
+
+function Hero() {
+  return (
+    <section>
+      <div></div>
+    </section>
+  );
+}
+
+export default Hero;
